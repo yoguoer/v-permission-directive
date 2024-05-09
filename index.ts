68e4aba85a4cdbf1521a00d58976cec9cd4f54ee
yoguoer <1269148583@qq.com>
2024-05-09 15:42:07 +0800

    pref ⚡️: 添加类型检查。

--- index.ts
+++ index.ts
@@ -2,13 +2,16 @@ import createPermissionDirective from './permission';
 import { initHasPermission } from './permission'
 
 // Vue 3 插件定义   
-const install = function (app, options = {
-  /** permissionList 系统预先配置的权限列表，包含所有权限信息
-  *   permissions 用户当前权限列表(服务端返回接口权限列表数据) 
-  */
-  permissionList: null,
-  permissions: null
-}) {
+const install = function (app: any, options: {
+  permissionList: Array<string> | null,
+  permissions: Array<string> | null
+} = {
+    /** permissionList 系统预先配置的权限列表，包含所有权限信息
+    *   permissions 用户当前权限列表(服务端返回接口权限列表数据) 
+    */
+    permissionList: null,
+    permissions: null
+  }) {
 
   // 初始化权限检查函数 
   const hasPermissions = initHasPermission(options);
@@ -17,7 +20,7 @@ const install = function (app, options = {
 
 
   // 提供全局的权限检查对象 
-  app.provide('hasPermissions', app.config.globalProperties.$hasPermissions); 
+  app.provide('hasPermissions', app.config.globalProperties.$hasPermissions);
 
   // 使用 hasPermissions 函数创建指令对象  
   const permissionDirective = createPermissionDirective(hasPermissions);
--- permission.ts
+++ permission.ts
@@ -9,44 +9,51 @@
   * permissionList 系统预先配置的权限列表，包含所有权限信息
   * permissions 用户当前权限列表(服务端返回接口权限列表数据) 
  */
-export function initHasPermission(options) {    
-  const { permissionList = null, permissions = null } = options;    
+export function initHasPermission(options: {
+  permissionList: Array<string> | null,
+  permissions: Array<string> | null
+}
+) {
+  const { permissionList = null, permissions = null } = options;
   // 返回一个函数，该函数接收一个权限对象并返回是否有权限    
-  return (permission) => {    
-    if (!permissionList || !permissions) {    
-      throw new Error('permissionList or permissions is null');    
-    }    
-    if (permission.module && permission.auth) {    
-      const value = permissionList[permission.module][permission.auth];    
-      return permissions.includes(value);    
-    }    
-    return false;    
-  };    
+  return (permission: {
+    module: string,
+    auth: string,
+  }) => {
+    if (!permissionList || !permissions) {
+      throw new Error('permissionList or permissions is null');
+    }
+    if (permission.module && permission.auth) {
+      const value = permissionList[permission.module][permission.auth];
+      return permissions.includes(value);
+    }
+    return false;
+  };
 }
 
 // 检查权限并执行相应的操作  
-function checkPermission(el, binding, hasPermissions) {  
-  if (typeof binding.value === 'object' && binding.value.module && binding.value.auth) {  
-    if (!hasPermissions(binding.value)) {  
-      el.style.display = 'none';  
-    } else {  
+function checkPermission(el: any, binding: any, hasPermissions: Function) {
+  if (typeof binding.value === 'object' && binding.value.module && binding.value.auth) {
+    if (!hasPermissions(binding.value)) {
+      el.style.display = 'none';
+    } else {
       el.style.display = ''; // 如果有权限，确保元素可见  
-    }  
-  }  
+    }
+  }
 }
 
 // 权限指令  
 // 创建一个返回指令对象的函数，该函数接受 hasPermissions 函数作为参数  
-export default function createPermissionDirective(hasPermissions) {  
-  return {  
-    mounted(el, binding) {  
-      checkPermission(el, binding, hasPermissions);  
-    },  
-    updated(el, binding) {  
-      checkPermission(el, binding, hasPermissions);  
-    }  
-  };  
-}  
+export default function createPermissionDirective(hasPermissions: Function) {
+  return {
+    mounted(el: any, binding: any) {
+      checkPermission(el, binding, hasPermissions);
+    },
+    updated(el: any, binding: any) {
+      checkPermission(el, binding, hasPermissions);
+    }
+  };
+}
 
 
 
